39242d5ea0fd2633de2adc558ed44c46d5b64a24
Amin394 <amin394@outlook.com>
2024-05-15 23:40:24 +0100

    site skeleton initiated

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <img className="header-logo" src="" alt="LOGO" />
+        <input className="header-search" type="text" placeholder=" Search ..." />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="topics">
+        <span>Topic 1</span>
+        <span>Topic 2</span>
+        <span>Topic 3</span>
+        <span>Topic 4</span>
+        <span>Topic 5</span>
+        <span>Topic 6</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="meta-separator">
+        <div className="blog">
+          <div className="blog-info">
+            <div className="blog-info-title">Blog 1</div>
+            <span className="blog-info-topic">Topic 3</span>
+            <span className="blog-info-writer">Writer 8</span>
+            <span className="blog-info-date">Today</span>
+          </div>
+          
+          <div className="blog-text">
+            <p>Hello, Word!</p>
+            <p>XYZ</p>
+          </div>
+        </div>
+
+        <div className="feed">
+          <div className="feed-blog">
+            <img className="feed-blog-thumbnail" src="" alt="THUMBNAIL" />
+            <div className="feed-blog-title">Blog 2</div>
+            <div className="feed-blog-writer">Writer 5</div>
+            <div className="feed-blog-date">Yesterday</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+      <input className="footer-newsletter" type="email" placeholder=" E-Mail" />
+        <div className="footer-link">Follow Us!</div>
+      </div>
     </>
   )
 }
