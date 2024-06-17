ba73f27aeb0c1925d4bbdef29bfd93092f19857b
Amin394 <amin394@outlook.com>
2024-06-17 20:09:24 +0100

    reworked navigation bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,11 @@
 import "./Navigation.css";
 
 function Navigation() {
-  return (
-    <>
-      <div className="header">
-        <img className="header-logo" src="" alt="LOGO" />
-        <input className="header-search" type="text" placeholder=" Search ..."/>
-      </div>
+  return (  
+    <div className="header">
+      <img className="header-logo" src="" alt="LOGO" />
 
-      <div className="topics">
+      <div className="header-topics">
         <span>Topic 1</span>
         <span>Topic 2</span>
         <span>Topic 3</span>
@@ -16,7 +13,9 @@ function Navigation() {
         <span>Topic 5</span>
         <span>Topic 6</span>
       </div>
-    </>
+
+      <input className="header-search" type="text" placeholder=" Search ..." />
+    </div>
   );
 }
 
