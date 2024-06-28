7e3dac2d80c5b4ae664ca2dd7f30c15c77ed23d3
Amin394 <amin394@outlook.com>
2024-06-28 21:36:53 +0100

    made content fetch from markdown files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
+import blogs from "./assets/markdown/.files_list";
 import Navigation from "./components/Navigation/Navigation";
-import Article from "./components/Content/Content";
+import Content from "./components/Content/Content";
 import Feed from "./components/Feed/Feed";
+import { useState } from "react";
 
 function App() {
+  const [currentBlog, setCurrentBlog] = useState(0);
+  const blogData = blogs[currentBlog];
+  console.log(currentBlog);
   return (
     <>
       <Navigation />
       <div className="separator">
-        <Article />
-        <Feed />
+        <Content blogData={blogData} />
+        <Feed setBlog={setCurrentBlog} />
       </div>
     </>
   );
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,34 +2,24 @@ import "./Content.css";
 import useFetch from "../../utilities/hooks/useFetch";
 import Markdown from "react-markdown";
 
-function Blog() {
-  const { data, loading } = useFetch("src/assets/markdown/The_Last_Title.md");
-
+function Content({ blogData }) {
+  const { data, loading } = useFetch("src/assets/markdown/" + blogData.path + ".md");
   if (loading) return <div className="loadingSpinner">Loading ...</div>;
-
-  const patternHead = /---([\s\S]+)---/;
-  const patternInfo = /:(.*?[^:])?(?=$|\n)/g;
-
-  const blogHead = data.match(patternHead)[1];
-  const blogBody = data.replace(data.match(patternHead)[0], "");
-  let blogInfo = blogHead.match(patternInfo);
-
-  console.log(blogInfo);
 
   return (
     <div className="blog">
       <div className="blog-info">
-        <div className="blog-info-title">{blogInfo[0]}</div>
-        <span className="blog-info-topic">{blogInfo[1]}</span>
-        <span className="blog-info-writer">{blogInfo[2]}</span>
-        <span className="blog-info-date">{blogInfo[3]}</span>
+        <h1 className="blog-info-title">{blogData.title}</h1>
+        <span className="blog-info-topic">{blogData.topic}</span>
+        <span className="blog-info-author">{blogData.author}</span>
+        <span className="blog-info-date">{blogData.date}</span>
       </div>
 
       <div className="blog-text">
-        <Markdown>{blogBody}</Markdown>
+        <Markdown>{data}</Markdown>
       </div>
     </div>
   );
 }
 
-export default Blog;
+export default Content;
